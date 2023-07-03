89e7e5fddc5d2d65c78586f9c498442227e4e893
mohamedisakr <mohamedisakr@gmail.com>
2023-07-03 10:49:21 +0300

    refactoring code & test

---
+++ program_efficiency/binary_search.py
@@ -0,0 +1,71 @@
+import unittest
+
+
+def binary_search(arr: list[int], target: int) -> int:
+    """
+    Performs a binary search on a sorted array to find the index of 
+    the target element.
+
+    Parameters
+    ----------
+    arr : list
+        A sorted list of comparable elements.
+    target : int
+        The element to search for in the array.
+
+    Returns
+    -------
+    int or None
+        The index of the target element in the array, or None if not found.
+
+    Raises
+    ------
+    ValueError: If the array is empty.
+    ValueError: If the array is not sorted.
+    TypeError: If the target element is not comparable to the elements in 
+               the array.
+
+    Examples
+    --------
+    >>> binary_search([1, 2, 3, 4, 5], 3)
+    2
+    >>> binary_search([1, 2, 3, 4, 5], 6)
+    None
+    >>> binary_search([], 1)
+    ValueError: array must not be empty
+    >>> binary_search([2, 1, 3], 1)
+    ValueError: array must be sorted
+    >>> binary_search([1, 2, 3], "a")
+    TypeError: '<' not supported between instances of 'str' and 'int'
+    """
+    # check for pre-condition
+    # check if arr is empty
+    if len(arr) == 0:
+        return None
+
+    if not isinstance(target, int):
+        raise TypeError(f"target must be an int, not {type(target)}")
+
+    if all(not isinstance(item, int) for item in arr):
+        raise TypeError(f"item must be an int, not {type(target)}")
+
+    # check if target value less than smallest item in the array
+    if target < arr[0]:
+        return None
+
+    # check if target value greater than largest item in the array
+    if target > arr[len(arr)-1]:
+        return None
+
+    # assume arr is sorted in ascending order
+    low = 0
+    high = len(arr) - 1
+    while low <= high:
+        mid = (low + high) // 2
+        if arr[mid] == target:
+            return mid  # found the target, return its index
+        elif arr[mid] < target:
+            low = mid + 1  # target is in the right half
+        else:
+            high = mid - 1  # target is in the left half
+    return None  # target not found
---
+++ tests/binary_search_test.py
@@ -0,0 +1,69 @@
+from unittest import TestCase, main
+from program_efficiency.binary_search import binary_search
+
+
+class TestBinarySearch(TestCase):
+
+    def test_empty_array(self):
+        # test case: empty array
+        self.assertEqual(binary_search([], 5), None)
+
+    def test_single_element_array(self):
+        # test case: single element array
+        self.assertEqual(binary_search([5], 5), 0)
+        self.assertEqual(binary_search([5], 6), None)
+
+    def test_multiple_element_array(self):
+        # test case: multiple element array
+        arr = [1, 3, 5, 7, 9]
+        self.assertEqual(binary_search(arr, 1), 0)
+        self.assertEqual(binary_search(arr, 9), 4)
+        self.assertEqual(binary_search(arr, 5), 2)
+
+    def test_array_with_non_exist_target(self):
+        arr = [1, 3, 5, 7, 9]
+        self.assertEqual(binary_search(arr, 4), None)
+        self.assertEqual(binary_search(arr, -1), None)
+        self.assertEqual(binary_search(arr, 10), None)
+
+    def test_array_with_duplicate_elements(self):
+        """ test case: array with duplicate elements"""
+        arr = [1, 2, 2, 3, 4, 4, 5]
+        self.assertEqual(binary_search(arr, 2), 1)
+        self.assertEqual(binary_search(arr, 6), None)
+        # self.assertEqual(binary_search(arr, 4), 4)
+
+    def test_array_with_negative_elements(self):
+        """ array with negative elements"""
+        arr = [-5, -3, -1, 0, 2, 4]
+        self.assertEqual(binary_search(arr, -3), 1)
+        self.assertEqual(binary_search(arr, 0), 3)
+        self.assertEqual(binary_search(arr, -6), None)
+
+    def test_array_with_mixed_types_of_elements(self):
+        """ array with mixed types of elements"""
+        arr = [1, "a", True, None, 3.14]
+        self.assertRaises(TypeError, binary_search, arr, "a")
+        # self.assertRaises(TypeError, binary_search, arr, True)
+        # self.assertRaises(TypeError, binary_search, arr, None)
+
+    # def test_array_with_mixed_types_of_elements_2(self):
+    #     """ array with mixed types of elements"""
+    #     arr = [1, "a", True, None, 3.14]
+    #     with self.assertRaises(TypeError):
+    #         binary_search(arr, True)
+    #     # self.assertRaises(TypeError, binary_search, arr, True)
+
+    def test_array_with_mixed_types_of_elements_3(self):
+        """ array with mixed types of elements"""
+        arr = [1, "a", True, None, 3.14]
+        self.assertRaises(TypeError, binary_search, arr, None)
+
+    def test_invalid_input(self):
+        # test case: invalid input
+        self.assertRaises(TypeError, binary_search, None, 5)
+        self.assertRaises(TypeError, binary_search, "hello", "e")
+
+
+if __name__ == '__main__':
+    main()
